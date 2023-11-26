812e3b0be1bae448ab0eecfe175c63b74ee00648
1dayluo <mar1sadaze@qq.com>
2023-11-26 19:47:18 +0800

    update

--- webhook.js
+++
@@ -1,7 +0,0 @@
-const { Application, Router } = require('@cfworker/web');
-const createTelegrafMiddleware = require('cfworker-middleware-telegraf');
-
-
-const router = new Router();
-router.post(`/${process.env.UUID}`, createTelegrafMiddleware(bot));
-new Application().use(router.middleware).listen();
